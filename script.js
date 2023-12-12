ce1bb1a927222ebf17e574e6eca91fb0003110dc
Andrii <andreychaika87@gmail.com>
2023-12-12 23:25:17 +0100

    start

---
+++ script.js
@@ -0,0 +1,19 @@
+const numberOfFilms = +prompt("Скільки фільмів ви вже переглянули?", "");
+
+const personalMovieDB = {
+  count: numberOfFilms,
+  movies: {},
+  actors: {},
+  ganres: [],
+  privat: false,
+};
+
+const a = prompt("Один з останніх переглянутих фільмів?", ""),
+  b = prompt("На скільки оцінюєте фільм?", ""),
+  c = prompt("Один з останніх переглянутих фільмів?", ""),
+  d = prompt("На скільки оцінюєте фільм?", "");
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
