d263889c003080a72a38ad4f6add53e2f2426d04
Andrii <andreychaika87@gmail.com>
2023-12-21 17:21:08 +0100

    script change, add function

--- script.js
+++ script.js
@@ -1,19 +1,58 @@
-const numberOfFilms = +prompt("Скільки фільмів ви вже переглянули?", "");
+let numberOfFilms;
+
+function start() {
+  numberOfFilms = +prompt("Скільки фільмів ви вже переглянули?", "");
+
+  while (numberOfFilms == "" || numberOfFilms == null || isNaN(numberOfFilms)) {
+    numberOfFilms = +prompt("Скільки фільмів ви вже переглянули?", "");
+  }
+}
+
+start();
 
 const personalMovieDB = {
   count: numberOfFilms,
   movies: {},
   actors: {},
-  ganres: [],
+  genres: [],
   privat: false,
 };
 
-const a = prompt("Один з останніх переглянутих фільмів?", ""),
-  b = prompt("На скільки оцінюєте фільм?", ""),
-  c = prompt("Один з останніх переглянутих фільмів?", ""),
-  d = prompt("На скільки оцінюєте фільм?", "");
+function rememberMyFilms() {
+  for (let i = 0; i < 2; i++) {
+    const lastFilm = prompt("Один з останніх переглянутих фільмів?", "");
+    const ratingFilm = prompt("На скільки оцінюєте фільм?", "");
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+    if (
+      lastFilm != null &&
+      ratingFilm != null &&
+      lastFilm != "" &&
+      ratingFilm != "" &&
+      lastFilm.length < 50
+    ) {
+      personalMovieDB.movies[lastFilm] = ratingFilm;
+      console.log("ok");
+    } else {
+      console.log("error");
+      i--;
+    }
+  }
+}
+
+rememberMyFilms();
+
+function detectPersonalLevel() {
+  if (personalMovieDB.count < 10) {
+    console.log("ви не кіноман");
+  } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+    console.log("ви класичний глядач");
+  } else if (personalMovieDB.count >= 30) {
+    console.log("кіноман");
+  } else {
+    console.log("error");
+  }
+}
+
+detectPersonalLevel();
 
 console.log(personalMovieDB);
